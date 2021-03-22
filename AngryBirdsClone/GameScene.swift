97680179d5c1bdc66b6e19f5992b2b4c89bcfc50
Ravisara S. Kariyawasam <ravisara@gmail.com>
2021-03-22 12:44:12 +0000

    Configures the five boxes in the scene with a physics body. Also, the bird and the boxes don't go off of the screen now. Completes section 16 unit 205.

--- AngryBirdsClone/GameScene.swift
+++ AngryBirdsClone/GameScene.swift
@@ -10,79 +10,104 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
+    var angryBird = SKSpriteNode()
+    var box1 = SKSpriteNode()
+    var box2 = SKSpriteNode()
+    var box3 = SKSpriteNode()
+    var box4 = SKSpriteNode()
+    var box5 = SKSpriteNode()
+        
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+//        let textureToUse = SKTexture(imageNamed: "bird")
+//        bird2 = SKSpriteNode(texture: textureToUse)
+//        bird2.position = CGPoint(x: 0, y: 0)
+//        bird2.zPosition = 1
+//        bird2.size = CGSize(width: 300, height: 300)
+//        bird2.position = CGPoint(x: -view.frame.width / 2, y: -view.frame.height / 3)
+//        bird2.size = CGSize(width: view.frame.width / 7, height: view.frame.height / 5)
+//        self.addChild(bird2)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        // Scene related
+        self.physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        self.scene?.scaleMode = .aspectFit
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        // Bird
+        angryBird =  childNode(withName: "bird") as! SKSpriteNode
+        let physicsBodyOfBird = SKPhysicsBody(circleOfRadius: angryBird.size.height / 3)
+        physicsBodyOfBird.affectedByGravity = true
+        physicsBodyOfBird.isDynamic = true
+        physicsBodyOfBird.mass = 1000
+        angryBird.physicsBody = physicsBodyOfBird
+        
+        // Boxes
+        let pbWidthAdjuster: Float = 8
+        let pbHeightAdjuster = 8
+        let textureForAllBoxes = SKTexture(imageNamed: "brick")
+        let boxSize = CGSize(width: textureForAllBoxes.size().width/CGFloat(pbWidthAdjuster), height: textureForAllBoxes.size().height/CGFloat(pbHeightAdjuster))
+        
+        box1 = childNode(withName: "box1") as! SKSpriteNode
+        box1.physicsBody = SKPhysicsBody(rectangleOf: boxSize)
+        box1.physicsBody?.allowsRotation = true
+        box1.physicsBody?.affectedByGravity = true
+        box1.physicsBody?.mass = 0.4
+        
+        box2 = childNode(withName: "box2") as! SKSpriteNode
+        box2.physicsBody = SKPhysicsBody(rectangleOf: boxSize)
+        box2.physicsBody?.allowsRotation = true
+        box2.physicsBody?.affectedByGravity = true
+        box2.physicsBody?.mass = 0.4
+        
+        box3 = childNode(withName: "box3") as! SKSpriteNode
+        box3.physicsBody = SKPhysicsBody(rectangleOf: boxSize)
+        box3.physicsBody?.allowsRotation = true
+        box3.physicsBody?.affectedByGravity = true
+        box3.physicsBody?.mass = 0.4
+        
+        box4 = childNode(withName: "box4") as! SKSpriteNode
+        box4.physicsBody = SKPhysicsBody(rectangleOf: boxSize)
+        box4.physicsBody?.allowsRotation = true
+        box4.physicsBody?.affectedByGravity = true
+        box4.physicsBody?.mass = 0.4
+        
+        box5 = childNode(withName: "box5") as! SKSpriteNode
+        box5.physicsBody = SKPhysicsBody(rectangleOf: boxSize)
+        box5.physicsBody?.allowsRotation = true
+        box5.physicsBody?.affectedByGravity = true
+        box5.physicsBody?.mass = 0.4
+                
     }
     
-    
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+        
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+        
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        
     }
-    
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
     }
+    
 }
